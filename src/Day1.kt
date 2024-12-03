e4cfbddce3b399b5f117f629afaba222775f3799
subdash <dhrs.dev@gmail.com>
2024-12-03 16:03:33 -0600

    Refactor day 1 to use priority queues

--- src/Day1.kt
+++ src/Day1.kt
@@ -1,5 +1,6 @@
 import java.io.File
 import java.io.InputStream
+import java.util.PriorityQueue
 import kotlin.math.abs
 
 const val PATH_TO_INPUT = "resources/puzzle_input.txt"
@@ -7,8 +8,8 @@ const val PATH_TO_INPUT = "resources/puzzle_input.txt"
 fun day1(): Long {
     val inputStream: InputStream = File(PATH_TO_INPUT).inputStream()
     val lineList = mutableListOf<String>()
-    val left = mutableListOf<Int>()
-    val right = mutableListOf<Int>()
+    val left = PriorityQueue<Int>()
+    val right = PriorityQueue<Int>()
 
     inputStream.bufferedReader().forEachLine { lineList.add(it) }
     lineList.forEach {
@@ -17,13 +18,10 @@ fun day1(): Long {
         right.add(Integer.parseInt(split.last()))
     }
 
-    left.sort()
-    right.sort()
-
     var distance: Long = 0
 
-    for (i in left.indices) {
-        distance += abs(left[i] - right[i])
+    while (left.isNotEmpty()) {
+        distance += abs(left.poll().toLong() - right.poll().toLong())
     }
 
     return distance
